b60bc505217d55f929fde0129d216dc5cd469fb7
Ope Omiwade <opeomiwade21@gmail.com>
2024-03-07 10:37:46 +0000

    Commit1

--- EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
+++ EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
@@ -2,46 +2,43 @@ package com.example;
 
 import java.util.Scanner;
 
-public class EmployeeSalaryCalculator 
-{
-	//Variable Declaration
-	final static int standardWorkingHours = 40;
-	static String employeeID;
-	static String employeeName;
-	static int hoursWorked;
-	static int  hourlyRate;
-	static int basicPay;
-	static int overTime;
-	static int overTimeRate;
-	static int overTimePay;
-	static int grossSalary;
-	
-	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		//Input
-		System.out.println("Enter an Employee ID: ");
-		employeeID = sc.nextLine();
-		
-		System.out.println("Enter an Employee Name: ");
-		employeeName = sc.nextLine();
-		
-		System.out.println("Enter the no. of Hours Worked: ");
-		hoursWorked = sc.nextInt();
-		
-		System.out.println("Enter the Hourly Rate: ");
-		hourlyRate = sc.nextInt();
-		
-		salaryCalculator(hoursWorked,hourlyRate);
-		displayPaySlip();
-	}
+public class EmployeeSalaryCalculator {
+    //Variable Declaration
+    final static int standardWorkingHours = 40;
+    static String employeeID;
+    static String employeeName;
+    static int hoursWorked;
+    static int hourlyRate;
+    static int basicPay;
+    static int overTime;
+    static int overTimeRate;
+    static int overTimePay;
+    static int grossSalary;
 
-	static int salaryCalculator(int HW,int HR)
-	{
-		hourlyRate = HR;
-		hoursWorked = HW;
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+
+        //Input
+        System.out.print("Enter an Employee ID: ");
+        employeeID = sc.nextLine();
+
+        System.out.print("Enter an Employee Name: ");
+        employeeName = sc.nextLine();
+
+        System.out.print("Enter the no. of Hours Worked: ");
+        hoursWorked = sc.nextInt();
+
+        System.out.print("Enter the Hourly Rate: ");
+        hourlyRate = sc.nextInt();
+
+        salaryCalculator(hoursWorked, hourlyRate);
+        displayPaySlip();
+    }
+
+    static int salaryCalculator(int HW, int HR) {
+        hourlyRate = HR;
+        hoursWorked = HW;
 		
 		/*
 		All new code must be added between the commented lines below.
@@ -56,46 +53,42 @@ public class EmployeeSalaryCalculator
 		*/
 
 
-				if(HW <= standardWorkingHours)
-				{
-					//YOUR CODE STARTS HERE
-					//  grossSalary = HR * HW;
-					//  basicPay = HR * HW;
+        if (HW <= standardWorkingHours) {
+            //YOUR CODE STARTS HERE
+            grossSalary = HR * HW;
+            basicPay = HR * HW;
 
-					//YOUR CODE ENDS HERE
-				}
-				else
-				{
-					//YOUR CODE STARTS HERE
-					// basicPay = HR * standardWorkingHours;
-					// overTimeRate = 2 * HR;
-					// overTime = HW - standardWorkingHours;
-					// overTimePay = overTimeRate * overTime;
-					// grossSalary = basicPay + overTimePay;
+            //YOUR CODE ENDS HERE
+        } else {
+            //YOUR CODE STARTS HERE
+            basicPay = HR * standardWorkingHours;
+            overTimeRate = 2 * HR;
+            overTime = HW - standardWorkingHours;
+            overTimePay = overTimeRate * overTime;
+            grossSalary = basicPay + overTimePay;
 
-					//YOUR CODE ENDS HERE
-				}
-				return grossSalary;
-	}
-	
-	static void displayPaySlip()
-	{
-		
-		System.out.println("===========================================");
-		System.out.println("                                     ");
-		System.out.println("================PAYSLIP==============");
-		System.out.println("                                     ");
+            //YOUR CODE ENDS HERE
+        }
+        return grossSalary;
+    }
 
-		System.out.println("Employee ID    :       " + employeeID);
-		System.out.println("Employee Name  :       " + employeeName);
-		System.out.println("Hours Worked   :       " + hoursWorked);
-		System.out.println("Hourly Rate    :       " + hourlyRate);
-		System.out.println("Overtime       :       " + overTime);
-		System.out.println("Overtime Rate  :       " + overTimeRate);
-		System.out.println("Overtime Pay   :       " + overTimePay);
-		System.out.println("Basic Pay      :       " + basicPay);
-		System.out.println("                                     ");
-		System.out.println("Gross Salary   :       " + grossSalary);
-		
-	}
+    static void displayPaySlip() {
+
+        System.out.println("===========================================");
+        System.out.println("                                     ");
+        System.out.println("================PAYSLIP==============");
+        System.out.println("                                     ");
+
+        System.out.println("Employee ID    :       " + employeeID);
+        System.out.println("Employee Name  :       " + employeeName);
+        System.out.println("Hours Worked   :       " + hoursWorked);
+        System.out.println("Hourly Rate    :       " + hourlyRate);
+        System.out.println("Overtime       :       " + overTime);
+        System.out.println("Overtime Rate  :       " + overTimeRate);
+        System.out.println("Overtime Pay   :       " + overTimePay);
+        System.out.println("Basic Pay      :       " + basicPay);
+        System.out.println("                                     ");
+        System.out.println("Gross Salary   :       " + grossSalary);
+
+    }
 }
